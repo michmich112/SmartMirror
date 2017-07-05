c84d787384253da2959cc9aae6b45bd331144d6f
Michel Can <mchl.aauee.98@gmail.com>
2017-07-05 08:17:57 -0400

    beginning pygame Code

--- Graphics.py
+++ Graphics.py
@@ -1,5 +1,13 @@
-import pygame
+import pygame, sys
 
 pygame.init()
-Surface = pygame.display.set_mode(500,500)
-Surface.fill(120,120,120)
+Surface = pygame.display.set_mode((500,500),0,32)
+pygame.display.set_caption("Mirror Test")
+Surface.fill((120,120,120))
+
+while True:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			sys.exit()
+	pygame.display.update()
